ad0070d68c73caad2bfaa8e815da250d3ac92fe9
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-01-03 21:53:35 +0100

    Add xcode 8

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -4,4 +4,8 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
   sudo -E apt-get update -qq
   sudo -E apt-get install -y qt5-default libmagick++-dev
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+  brew update
+  brew install imagemagick --with-magick-plus-plus
+  brew install qt@5.5
 fi
