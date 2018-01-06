dedce1bf9aef6f327d701e92c716e94f76124678
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-01-06 20:43:47 +0100

    Fix OSX build

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -6,7 +6,14 @@ cd build
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
   cmake -DCMAKE_C_COMPILER=$COMPILER_CC -DCMAKE_CXX_COMPILER=$COMPILER_CXX ..
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  cmake -DCMAKE_C_COMPILER=$COMPILER_CC -DCMAKE_CXX_COMPILER=$COMPILER_CXX -DImageMagick_Magick++_LIBRARY="$( pkg-config --libs Magick++ )" -DCMAKE_PREFIX_PATH="`brew --prefix qt`" ..
+  # CMake's find_package for ImageMagick is not setting ImageMagick_Magick++_LIBRARY 
+  # for the Magick++ component. Here is an evil hack to solve this...
+
+  magick++_folder=`brew --prefix imagemagick`/lib/
+  magick++_lib=`ls $magick++_folder | grep ++ | grep HDRI.dylib`
+  magick++_path=$magick++_folder$magick++_lib
+
+  cmake -DCMAKE_C_COMPILER=$COMPILER_CC -DCMAKE_CXX_COMPILER=$COMPILER_CXX -DImageMagick_Magick++_LIBRARY=$magick++_path -DCMAKE_PREFIX_PATH="`brew --prefix qt`" ..
 fi
 
 cmake --build .
