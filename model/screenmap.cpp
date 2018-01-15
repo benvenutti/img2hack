debaa3263c93f4af09b96a2323c8cd85c5f16e00
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-01-15 21:17:19 +0100

    Format all files in project

--- model/screenmap.cpp
+++ model/screenmap.cpp
@@ -6,55 +6,51 @@ using namespace Magick;
 using namespace std;
 
 ScreenMap::ScreenMap(Image img)
-    : image(img)
-{
-    readImage();
+: image(img) {
+  readImage();
 }
 
-void ScreenMap::add(int16_t word, int address)
-{
-    auto it = words.find(word);
+void ScreenMap::add(int16_t word, int address) {
+  auto it = words.find(word);
 
-    if (it != words.end())
-        words.at(word).insert(address);
-    else {
-        set<int> s;
-        s.insert(address);
-        words.insert(pair< int16_t, set<int> >(word, s));
+  if (it != words.end())
+    words.at(word).insert(address);
+  else {
+    set<int> s;
+    s.insert(address);
+    words.insert(pair<int16_t, set<int>>(word, s));
+  }
+}
+
+map<int16_t, set<int>> ScreenMap::getMap() {
+  return words;
+}
+
+void ScreenMap::readImage() {
+  int counter = 0;
+  int16_t word = 0;
+  uint16_t mask = 1;
+  int adr = 0;
+
+  for (int i = 0; i < HACK::SCREEN_HEIGHT; ++i) {
+    for (int j = 0; j < HACK::SCREEN_WIDTH; ++j) {
+
+      ColorRGB c(image.pixelColor(j, i));
+
+      if (c.red() * QuantumRange == 0) {
+        word = word | mask;
+      }
+
+      mask <<= 1;
+      counter++;
+
+      if (counter > HACK::WORD_SIZE - 1) {
+        add(word, adr);
+        counter = 0;
+        word = 0;
+        mask = 1;
+        adr++;
+      }
     }
-}
-
-map< int16_t, set<int> > ScreenMap::getMap()
-{
-    return words;
-}
-
-void ScreenMap::readImage()
-{
-    int counter = 0;
-    int16_t word = 0;
-    uint16_t mask = 1;
-    int adr = 0;
-
-    for (int i = 0; i < HACK::SCREEN_HEIGHT; ++i) {
-        for (int j = 0; j < HACK::SCREEN_WIDTH; ++j) {
-
-            ColorRGB c(image.pixelColor(j, i));
-
-            if (c.red() * QuantumRange == 0) {
-                word = word | mask;
-            }
-
-            mask <<= 1;
-            counter++;
-
-            if (counter > HACK::WORD_SIZE - 1) {
-                add(word, adr);
-                counter = 0;
-                word = 0;
-                mask = 1;
-                adr++;
-            }
-        }
-    }
+  }
 }
--- view/aboutdialog.h
+++ view/aboutdialog.h
@@ -7,21 +7,20 @@ namespace Ui {
 class AboutDialog;
 }
 
-class AboutDialog : public QDialog
-{
-    Q_OBJECT
+class AboutDialog : public QDialog {
+  Q_OBJECT
 
 public:
-    explicit AboutDialog(QString url, QWidget *parent = 0);
-    ~AboutDialog();
+  explicit AboutDialog(QString url, QWidget* parent = 0);
+  ~AboutDialog();
 
 private slots:
-    void on_btLink_clicked();
-    void on_btOK_clicked();
+  void on_btLink_clicked();
+  void on_btOK_clicked();
 
 private:
-    Ui::AboutDialog *ui;
-    QString url;
+  Ui::AboutDialog* ui;
+  QString url;
 };
 
 #endif // ABOUTDIALOG_H
